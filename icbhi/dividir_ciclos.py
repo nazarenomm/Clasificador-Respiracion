5178b14f8b8a3102c3fb3fdcbc5e26d3d331caa9
nazarenomm <nazarenomm98@gmail.com>
2025-10-08 21:05:43 -0300

    segmentación mejorada + comienzo de armado de datasets finales

--- icbhi/dividir_ciclos.py
+++ icbhi/dividir_ciclos.py
@@ -5,7 +5,7 @@ import pandas as pd
 # Paths
 DATASET_DIR = "./icbhi/ICBHI_final_database"
 ANNOTATIONS_DIR = os.path.join(DATASET_DIR, "notaciones")
-OUTPUT_DIR = "./icbhi/ICBHI_ciclos"
+OUTPUT_DIR = "./dataset/ciclos"
 CSV_PATH = "./icbhi/ICBHI_ciclos/ICBHI_ciclos_metadata.csv"
 
 os.makedirs(OUTPUT_DIR, exist_ok=True)
