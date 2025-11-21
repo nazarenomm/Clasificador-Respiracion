9eb5edc97582027d51777b079d470a7d77dd9edc
nazarenomm <nazarenomm98@gmail.com>
2025-11-21 13:52:49 -0300

    reestructuracion data

--- config.py
+++ config.py
@@ -2,25 +2,21 @@
 Configuraciones globales para el proyecto.
 """
 
-# Audio
+# Preprocesamiento
 SAMPLING_RATE = 16_000
 FRAME_LENGTH = 1_024
 HOP_LENGTH = 256 # puede ser menos para mayor resolucion temporal y precision en la deteccion de crepitaciones.
 MAX_DURATION = 6  # segundos, duración máxima de un ciclo respiratorio (inspiración + espiración)
 MIN_DURATION = 1  # segundos, duración mínima de un ciclo respiratorio (inspiración + espiración)
-
-# Filtros pasa banda
-HIGH_CUT = 4_000
-LOW_CUT = 100 # supuestamente las frecuencias de interes estan entre 250-350 y 2000-4000 Hz
-
-# Mel Espectrograma
 N_MELS = 128
 N_FFT = 1_024
 MAX_LENGTH = 1 + int((MAX_DURATION * SAMPLING_RATE - N_FFT) // HOP_LENGTH)
 PADDING_MODE = 'center'  # 'end', 'start' o 'center'
-
-# Features
 N_MFCC = 40
+
+# Filtros pasa banda
+HIGH_CUT = 4_000
+LOW_CUT = 100
 
 # Entrenamiento
 SEED = 42
